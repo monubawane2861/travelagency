0650401cd9f34a530e52ac4f7e97957425701fca
monubawane2861 <monubawane2861>
2025-07-12 14:51:58 +0530

    seven

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-// src/App.jsx
 import React from "react";
 import {
   BrowserRouter as Router,
@@ -8,13 +7,13 @@ import {
 } from "react-router-dom";
 import "./App.css";
 import { useEffect } from "react";
-// Import pages
+
 import Home from "./components/pages/Home/Home";
 import About from "./components/pages/About/About";
 import AvailableCar from "./components/pages/AvailableCar/AvailableCar";
 import Contact from "./components/pages/Contact/Contact";
 import Review from "./components/pages/Review/Review";
-// import CallNow from "./components/pages/callnowchatboot/CallNow";
+import CallNow from "./components/pages/callnowchatboot/CallNow";
 import Book from "./components/pages/book/Book";
 import Services from "./components/pages/services/Services";
 import NotFound from "./components/pages/NotFound/NotFound";
@@ -41,7 +40,7 @@ function App() {
             <Route path="/cars" element={<AvailableCar />} />
             <Route path="/contact" element={<Contact />} />
             <Route path="/reviews" element={<Review />} />
-            {/* <Route path="/call" element={<CallNow />} /> */}
+            <Route path="/call" element={<CallNow />} />
             <Route path="/book" element={<Book />} />
             <Route path="/services" element={<Services />} />
             <Route path="*" element={<NotFound />} />
